73bcb61dab2c9a53905486d38b504ace6168dfbe
Hebi Li <lihebi.com@gmail.com>
2020-07-10 21:20:03 +0800

    add fitboard

---
+++ tests/fitboard.rkt
@@ -0,0 +1,213 @@
+;; #lang racket
+#lang s-exp "../src/splicing.rkt"
+
+(require "../src/sch.rkt"
+         "../src/library.rkt"
+         "../src/library-IC.rkt"
+         "../src/utils.rkt"
+         "../src/pict-utils.rkt"
+         "../src/common.rkt"
+
+         "../src/place.rkt"
+
+         "../src/fp-kicad.rkt"
+         "../src/library-io.rkt"
+
+         (except-in pict
+                    rotate
+                    vl-append
+                    vc-append
+                    vr-append
+                    ht-append
+                    hc-append
+                    hb-append
+                    htl-append
+                    hbl-append)
+
+         "../src/atom-pict-wrapper.rkt"
+
+         json
+         (for-syntax syntax/parse
+                     racket/string))
+
+(define (sw spacing)
+  (footprint->pict
+   (fp-switch-keyboard spacing 'pcb)))
+
+(module+ test
+  (fp-switch-keyboard 1 'pcb)
+  (sw 1)
+  (sw 1.5))
+
+(define (key-name-filter name)
+  (if (string-prefix? name "k")
+      (substring name 1)
+      name))
+
+(define (create-switch-fn spacing)
+  (lambda (name)
+    (if (= spacing 0)
+        #f
+        (let ([atom (cherry spacing)])
+          (picted-atom! atom
+                        (cc-superimpose (atom->fp-pict atom)
+                                        (text
+                                         (key-name-filter (symbol->string name)))))))))
+
+(struct KeyboardMatrix
+  (rows))
+
+(define (keyboard-row mat i)
+  (filter identity (list-ref (KeyboardMatrix-rows mat) i)))
+(define (keyboard-col mat i)
+  (filter identity
+          (for/list ([row (KeyboardMatrix-rows mat)])
+            (list-ref row i))))
+(define (keyboard-xy mat x y)
+  (list-ref (list-ref (KeyboardMatrix-rows mat) x) y))
+
+(begin-for-syntax
+ (define-syntax-class key-spec
+   #:datum-literals (-)
+   (pattern - #:with name (car (generate-temporaries '(a)))
+            #:with spacing #'0)
+   (pattern ID:id #:with name #'ID
+            #:with spacing #'1)
+   (pattern (ID:id spacing0:number)
+            #:with name #'ID
+            #:with spacing #'spacing0)))
+
+(define-syntax (define-key-matrix stx)
+  (syntax-parse
+   stx
+   [(_ name ([key:key-spec ...] ...))
+    #'(begin
+        ;; 1. define the atoms
+        (define key.name ((create-switch-fn key.spacing) 'key.name))
+        ... ...
+        ;; 2. assign matrix to the variable
+        (define name (KeyboardMatrix (list (list key.name ...) ...))))]))
+
+(define-key-matrix matrix
+  ([(esc 1.5)    k1 k2 k3 k4    k5       -         -        k6 k7 k8  k9  k0   (backspace 1.5)]
+   [(tab 1.5)    Q  W  E  R     T        -         -        Y  U  I   O   P    (k\\ 1.5)]
+   [(caps 1.5)   A  S  D  F     G        lmid1 rmid1        H  J  K   L   k\;  (enter 1.5)]
+   [(lshift 1.5) Z  X  C  V     B        lmid2 rmid2        N  M  k\, k\. k\/  (rshift 1.5)]
+   [lctrl lfn lsuper lalt lTBD (lspace 2.75) - - (rspace 2.75) rTBD ralt rsuper rfn rctrl]))
+
+(module+ test
+  (KeyboardMatrix-rows matrix)
+  (keyboard-row matrix 1)
+  (keyboard-col matrix 6))
+
+(define (make-half left-or-right col1 col2 col3 col4 col5 col6 col7)
+  (let-values
+      ([(vr-append vl-append hb-append pi)
+        (case left-or-right
+          [(left)  (values vr-append vl-append hb-append pi)]
+          [(right) (values vl-append vr-append
+                           (lambda (arg . args)
+                             (hb-append (reverse (cons arg args)) ..))
+                           (- pi))])])
+    (let* ([padding (λ (unit)
+                      (ghost (rectangle 10 unit)))])
+      (parameterize ([default-append-spacing 20])
+        (rotate (hb-append (vr-append col1 ..)
+                           (vr-append col2 .. (padding 30))
+                           (vr-append col3 .. (padding 60))
+                           (vr-append col4 .. (padding 80))
+                           (vr-append col5 .. (padding 60))
+                           ;; col6
+                           (match (list col6 col7)
+                             [(list (list k5 t g b lspace) (list lmid1 lmid2))
+                              (vl-append (hb-append (vl-append  k5 t g b)
+                                                    (vl-append
+                                                     (rotate lmid1 (/ pi 2))
+                                                     (rotate lmid2 (/ pi 2))))
+                                         lspace
+                                         (padding 30))]))
+                (- (/ pi 10)))))))
+
+;; TODO I need to support two kinds of location.
+;; 1. functional pict
+;; 2. specify absolute location directly
+;; 3. possibly mixing the two?
+
+(define-Composite matrix-module
+  ;; FIXME these should be row[5] col[14], or get from matrix
+  #:external-pins (row1 row2 row3 row4 row5
+                        col1 col2 col3 col4 col5 col6 col7
+                        col8 col9 col10 col11 col12 col13 col14)
+  #:layout (hc-append -150
+                      (make-half 'left
+                                 (for/list ([i (range 7)])
+                                   (keyboard-col matrix i))
+                                 ..)
+                      (make-half 'right
+                                 (for/list ([i (reverse (range 7 14))])
+                                   (keyboard-col matrix i))
+                                 ..))
+  ;; connections
+  #:connect (for/list ([x (range 5)])
+              (filter-not
+               void?
+               (for/list ([y (range 14)])
+                 (let ([key (keyboard-xy matrix x y)]
+                       ;; FIXME this is ugly
+                       [xname (string->symbol (~a "row" (add1 x)))]
+                       [yname (string->symbol (~a "col" (add1 y)))])
+                   (when key
+                     (*- (pin-ref self yname) (diode) key (pin-ref self xname))))))))
+
+(module+ test
+ (Composite-pict matrix-module)
+ (Composite-nets matrix-module)
+
+ (define whole matrix-module)
+
+ (Composite-pict whole)
+ ;; TODO NOW rotation of fixed-location components
+ (define init-place (Composite->place-spec whole))
+ (Composite->pict whole
+                  (hash-ref init-place 'xs)
+                  (hash-ref init-place 'ys)))
+
+(module+ test-kicad
+  (define whole matrix-module)
+  (define place-result
+    (send-for-placement
+     ;; TODO the diearea should be specified only once
+     (Composite->place-spec whole
+                            #:place-nsteps 50
+                            #:place-nbins 300
+                            ;; When cycle increases, the temperature cools down,
+                            ;; and the later cycles are not very useful to
+                            ;; remove conflicts. Thus, for this application, I
+                            ;; might consider using only the first few cycles,
+                            ;; and use a large number of steps (per cycle)
+                            #:sa-ncycles 30
+                            #:sa-nsteps 2000
+                            #:sa-stepsize 10)))
+  (save-file (Composite->pict whole
+                              (hash-ref place-result 'xs)
+                              (hash-ref place-result 'ys))
+             "gh60.pdf")
+  (call-with-output-file "out.kicad_pcb"
+    #:exists 'replace
+    (λ (out)
+      (pretty-write (Composite->kicad-pcb whole
+                                          (hash-ref place-result 'xs)
+                                          (hash-ref place-result 'ys))
+                    out)))
+  (call-with-output-file "out.dsn"
+    #:exists 'replace
+    (λ (out)
+      (pretty-write (Composite->dsn whole
+                                    (hash-ref place-result 'xs)
+                                    (hash-ref place-result 'ys))
+                    out)))
+  ;; call command line tool to do routing
+  (current-directory)
+  (system "freerouting-1.4.4-executable.jar -de out.dsn -do out.ses -mp 5")
+  (system "ls"))
+
