b3be067175886315710f3e6961a597e4248c3b9a
Hebi Li <hebi@lihebi.com>
2020-11-06 15:32:45 +0000

    use 8082 for placement process management

--- bhdl-lib/bhdl/private/place.rkt
+++ bhdl-lib/bhdl/private/place.rkt
@@ -42,29 +42,30 @@
          ;; placement engine management
          start-placement-engine
          stop-placement-engine
-         status-placement-engine
-         output-placement-engine)
+;;          status-placement-engine
+;;          output-placement-engine
+         )
 
+;; I actually don't want to hold onto this, because I don't want to restart the server in each notebook session because it is slow until the TCP port is being listening, and the placement engine warm up is also slow. I'll just monitor the 8082 port.
 (define the-placement-process (make-parameter #f))
 
-(define (start-placement-engine [port 8082])
+(define (start-placement-engine [port 8082] [use-gpu #t])
   "Launch placement engine on port. If already running, do nothing."
   ;; check the engine running.
   ;; TOOD whether the engine should be shalled for other processes? Looks like yes, because we are using a port
   ;; TODO whether to launch different engines on different port?
   ;; TODO choose to use GPU or not
-  ;; TODO how to launch the process? Using costodian or process?
   ;; TODO this will be launched asynchronizely, and I need a way to monitor the output. This output is probably better shown in a separate window.
-  (when (not (the-placement-process))
-     ;; TODO change directory temporarily
-  (debug "Instantiating the julia project packages ..")
-  (shell "cd ~/bhdl/placement && julia --project -e 'import Pkg; Pkg.instantiate(); Pkg.precompile();'")
-  (debug "Starting the placement engine asynchronizely ..")
-;;         (list stdout stdin id stderr func)
-         ;; TODO how to redirect the stdout to a file?
-    (the-placement-process (process "cd ~/bhdl/placement && julia --project main.jl > ~/bhdl/output.txt 2>&1"))
-        ;; I actually wanted to wait a little bit until the process is listening on that port
-        (wait-for-8082)
+  (when (not (get-8082-processid))
+         ;; TODO change directory temporarily
+      (debug "Instantiating the julia project packages ..")
+      (shell "cd ~/bhdl/placement && julia --project -e 'import Pkg; Pkg.instantiate(); Pkg.precompile();'")
+      (debug "Starting the placement engine asynchronizely ..")
+    ;;         (list stdout stdin id stderr func)
+             ;; TODO how to redirect the stdout to a file?
+        (the-placement-process (process "cd ~/bhdl/placement && julia --project main.jl > ~/bhdl/output.txt 2>&1"))
+            ;; I actually wanted to wait a little bit until the process is listening on that port
+            (wait-for-8082)
       ))
 
 ;; if no process this kill will throw error
@@ -72,6 +73,7 @@
 
 
 (define (get-8082-processid)
+  ;; HACK I'm depending on the behavior that if the sting is not a number, string->number will return #f
   (string->number (string-trim (with-output-to-string (lambda () (shell "lsof -i :8082 | awk '{print $2}' | tail -n 1"))))))
 
 (define (stop-8082-process)
@@ -82,9 +84,8 @@
 (define (wait-for-8082)
   (for ([i (in-naturals)]
       #:break (get-8082-processid))
-     (display (~a "The TCP port is still " (get-8082-processid) " , wainting for 1 sec .. process status: "))
-     (status-placement-engine)
-     (flush-output)
+     (displayln (~a "The TCP port is still " (get-8082-processid) ", wainting for 1 sec .."))
+;;      (flush-output)
      (sleep 1))
   (display (~a "TCP port ready: " (get-8082-processid))))
 
@@ -92,18 +93,18 @@
 (define (stop-placement-engine)
   "Close placement engine."
   ;; TODO I would also run a ps -ef | grep .. or a lsof of 8022 for any existing process
-  (when (the-placement-process)
-        ((list-ref (the-placement-process) 4) 'kill)
-        (the-placement-process #f))
+;;   (when (the-placement-process)
+;;         ((list-ref (the-placement-process) 4) 'kill)
+;;         (the-placement-process #f))
   ;; actually stop the process on 8082
   (stop-8082-process))
 
-(define (status-placement-engine)
-  "Close placement engine."
-  (if (the-placement-process)
-      (begin
-        (displayln ((list-ref (the-placement-process) 4) 'status)))
-      (displayln "No active placement engine.")))
+;; (define (status-placement-engine)
+;;   "Close placement engine."
+;;   (if (the-placement-process)
+;;       (begin
+;;         (displayln ((list-ref (the-placement-process) 4) 'status)))
+;;       (displayln "No active placement engine.")))
 
 (define (output-placement-engine)
   (when (the-placement-process)
